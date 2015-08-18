6e7e6613e75e52c0de8d44b3b81ab3a2f059e222
Brian Borealis <brian.borealis@gmail.com>
2015-08-17 23:13:50 -0700

    update RockTest.php

--- tests/RockTest.php
+++ tests/RockTest.php
@@ -7,7 +7,7 @@
 
   require_once "src/Rock.php";
 
-  $server = 'mysql:host=localhost;dbname=to_do_test';
+  $server = 'mysql:host=localhost;dbname=inventory_tests';
   $username = 'root';
   $password = 'root';
   $DB = new PDO($server, $username, $password);
